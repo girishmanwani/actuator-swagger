4752e7cfab3a0f1f6e25833e6b417770f7cc5a03
Girish Manwani <girishmanwani@gmail.com>
2022-10-11 19:59:44 +0530

    Add files via upload

---
+++ src/main/java/com/example/actuatorservice/HelloWorldApplication.java
@@ -0,0 +1,35 @@
+package com.example.actuatorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+/*
+https://spring.io/guides/gs/actuator-service/
+http://localhost:9000/hello-world
+http://localhost:9001/actuator/health
+http://localhost:9000/swagger-ui.html		
+*/
+
+@SpringBootApplication
+@EnableSwagger2
+public class HelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldApplication.class, args);
+	}
+
+	
+	  @Bean public Docket api() { return new
+	  Docket(DocumentationType.SWAGGER_2).select()
+	  .apis(RequestHandlerSelectors.any()).paths(PathSelectors.any()).build(); }
+	 
+
+}
